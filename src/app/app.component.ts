1ef18cf57c19bb59a4ab9b82ba00010e2c50b2eb
Şamil Akpınar <samilakpinar6@gmail.com>
2026-02-13 23:07:21 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularproject';
+
+   hearts: any[] = [];
+  balloons: any[] = [];
+  showCard = false;
+
+  ngOnInit(): void {
+    this.generateHearts();
+    this.generateBalloons();
+
+    // 4 saniye sonra kart çıksın
+    setTimeout(() => {
+      this.showCard = true;
+    }, 4000);
+  }
+
+  generateHearts() {
+    for (let i = 0; i < 20; i++) {
+      this.hearts.push({
+        left: Math.random() * 100,
+        duration: 4 + Math.random() * 4
+      });
+    }
+  }
+
+  generateBalloons() {
+    for (let i = 0; i < 10; i++) {
+      this.balloons.push({
+        left: Math.random() * 100,
+        duration: 6 + Math.random() * 4
+      });
+    }
+  }
+
+blackout = false;
+showBigLove = false;
+
+startJoke() {
+
+  // ekran kararsın
+  this.blackout = true;
+
+  setTimeout(() => {
+    // ekran açılsın ve büyük yazı çıksın
+    this.blackout = false;
+    this.showBigLove = true;
+  }, 1500);
+}
 }
